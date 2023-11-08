2f6fd50c56645e92d864217e04f45736dcd8ec60
TahmidShawn7 <tahmidshawn7@gmail.com>
2023-11-08 21:43:12 +0600

    client side extra feature completed

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ async function run() {
 
 
 
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
